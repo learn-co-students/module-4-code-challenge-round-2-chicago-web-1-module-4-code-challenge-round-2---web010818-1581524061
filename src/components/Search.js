20c3ee17d77474c96f2ad05fb134f20c50e30d0e
porawiec <54656651+porawiec@users.noreply.github.com>
2020-02-12 11:34:21 -0600

    completed test

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Search = () => {
-  return (
-    <div className="ui huge fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  )
+class Search extends Component {
+  render(){
+    return (
+      <div className="ui huge fluid icon input">
+        <input
+          type="text"
+          placeholder={"Search your Recent Transactions"}
+          onChange={this.props.handleChange}
+        />
+        <i className="circular search link icon"></i>
+      </div>
+    )
+  }
 }
-
 export default Search
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+class TransactionsList extends Component {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
 
-        {"... your code here..."}
-
-      </tbody>
-    </table>
-  )
+renderTransaction = () => {
+  return this.props.transactions.map(tranObj => {
+    return (
+      <Transaction trans={tranObj} />
+    )
+  })
 }
+
+
+  render(){
+    // console.log(this.props.transactions)
+
+      return (
+        <table className="ui celled striped padded table">
+          <tbody>
+            <tr>
+              <th>
+                <h3 className="ui center aligned header">
+                  Posted At
+                </h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">
+                  Description
+                </h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">
+                  Category
+                </h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">
+                  Amount
+                </h3>
+              </th>
+            </tr>
+
+            {this.renderTransaction()}
+            
+
+          </tbody>
+        </table>
+      )
+    }
+  }
 
 export default TransactionsList
